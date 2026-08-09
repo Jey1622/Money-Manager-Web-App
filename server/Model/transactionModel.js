241a60be15127e30f1d291821b855cb56b40eac1
Jeyaraman S <jeyaramanapr22@gmail.com>
2026-08-09 22:06:58 +0530

    Modify the category type in transaction model

--- server/Model/transactionModel.js
+++ server/Model/transactionModel.js
@@ -12,7 +12,8 @@ const TransactionModel = new mongoose.Schema(
       min: 0,
     },
     category: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
       required: true,
     },
     account: {
