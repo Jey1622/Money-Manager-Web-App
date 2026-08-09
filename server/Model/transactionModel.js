7e93bff3470b6d180a009ed9135b58b49a4c301c
Jeyaraman S <jeyaramanapr22@gmail.com>
2026-08-09 19:42:49 +0530

    Created a transaction model

---
+++ server/Model/transactionModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const TransactionModel = new mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    account: {
+      type: String,
+      enum: ["Cash", "Account", "Card"],
+      required: true,
+    },
+    note: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+let model = mongoose.model("Transaction", TransactionModel);
+
+module.exports = model;
