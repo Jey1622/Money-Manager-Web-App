12c0d59dafe193e92b2c8e4cd649b4ba1be198c9
Jeyaraman S <jeyaramanapr22@gmail.com>
2026-09-02 16:46:08 +0530

    remove unwanted code

--- client/src/redux/slices/TransactionSlice.js
+++ client/src/redux/slices/TransactionSlice.js
@@ -1,33 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const transactionSlice = createSlice({
-//   name: "transaction",
-//   initialState: {
-//     transactions: [],
-//     loader: false,
-//   },
-//   reducers: {
-//     setTransactions(state, action) {
-//       return {
-//         ...state,
-//         transactions: action.payload,
-//         loading: false,
-//       };
-//     },
-//     setLoader(state, action) {
-//       return {
-//         ...state,
-//         loader: action.payload,
-//       };
-//     },
-//   },
-// });
-
-// const { actions, reducers } = transactionSlice;
-// export default reducers;
-
-// export const { setLoader, setTransactions } = actions;
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const transactionSlice = createSlice({
